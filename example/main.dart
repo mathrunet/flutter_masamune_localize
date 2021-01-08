fb2ea7cb3acf9e8b8d087d5a79e9116ab0736975
mathru <user@mathru.net>
2021-01-08 22:56:14 +0900

    バージョンアップ

--- example/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:masamune_localize/masamune_localize.dart';
-
-void main() async {
-  await Localize.initialize("Localization.csv");
-  String localized = Localize.get("Hello");
-  print(localized);
-}
